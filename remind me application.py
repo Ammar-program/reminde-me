e09a3ccdcc9393f369c68be5cfb67e6257abb78c
Ammar-program <84170554+Ammar-program@users.noreply.github.com>
2021-05-14 02:18:32 -0400

    Create remind me application.py

---
+++ remind me application.py
@@ -0,0 +1,15 @@
+import time
+
+print("what shall i remind you about?")
+
+text = str(input())
+
+print("in how many minutes?")
+
+local_time = float(input())
+
+local_time = local_time * 60
+
+time.sleep(local_time)
+
+print(local)
